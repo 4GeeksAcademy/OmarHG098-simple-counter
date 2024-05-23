e3c688ece971e54b68ec1b58099844cbef75f948
Omar Hidalgo <omar_ehr@outlook.com>
2024-05-23 01:04:59 +0000

    ADD counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,36 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { element } from "prop-types";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const Home = ({ counter }) => {
+  const count = counter.toString().padStart(6, "0");
+  const countArray = count.split("");
+  console.log(countArray);
+
+  return (
+    <div className="text-center my-5">
+      <div>
+        <div className="container text-center bg-dark text-light p-3 my-2">
+          <div className="row fs-1">
+            <div className="col bg-gradient mx-1">
+              <i className="far fa-clock"></i>
+            </div>
+            {countArray.map((element) =>{
+				return(<div className="col bg-gradient mx-1">{element}</div>)
+			})}
+          </div>
+        </div>
+      </div>
+      <a href="#" className="btn btn-success">
+        If you see this green button... bootstrap is working...
+      </a>
+      <p>
+        Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+        love!
+      </p>
+    </div>
+  );
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,8 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(()=>{
+    counter += 1;
+    ReactDOM.render(<Home counter={counter}/>, document.querySelector("#app"))
+}, 1000);
